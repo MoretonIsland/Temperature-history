cff8c75adf655a0a1e38dff18f697c8faacfccee
MoretonIsland <107469969+MoretonIsland@users.noreply.github.com>
2024-01-21 11:01:34 +0100

    Create home.php

---
+++ home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="common/style.css">
+    <link rel="stylesheet" type="text/css" href="home.css">.
+    <title>Temperature History</title>   
+    
+</head>
+<body>
+
+<nav>
+    <a href="home.php">Home</a>
+    <a href="avgtemp.php">Average temperature</a>
+    <a href="maxtemp.php">Maximum temperature</a>
+    <a href="mintemp.php">Minimum temperature</a>
+</nav>
+
+<div class="content">
+  <!-- Obsah hlavní stránky -->
+  <h1>Temperature trend 1948 - 2023 <br> Milwaukee, Wi</h1>
+  <p>Select a temperature data category from the left menu.</p>
+  
+ <!--
+<script>
+    // JavaScript loop to generate image tags
+    for (let i = 0; i < 1; i++) {
+      document.write('<img src="sky25.jpg" alt="Image" width="50" height="43">');
+    }
+</script>
+-->
+<img src="sky25.jpg" alt="Image" width="50" height="43" class="spaced">
+</body>
+</html>
