d9ded2ce9235f1f8de6dc0feb3bfe793c57928dd
MoretonIsland <107469969+MoretonIsland@users.noreply.github.com>
2024-01-21 11:17:59 +0100

    Create mintemp.php

---
+++ mintemp.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="chart5.js"></script>
+    <script src="chart6.js"></script>
+</head>
+
+<style>
+canvas {
+    display: block;
+    margin: 60px auto; /* Přidání mezery nad grafy */
+    width: 60vw; /* Nastavení šířky na 60% šířky okna prohlížeče */
+}
+
+#yearRange {
+        color:black;  /*barva nápisu 1948 - 2023*/
+}
+
+#Average {
+   text-decoration: underline;  /*podtržení nadpisu h2 */
+}
+.sidebar {
+    background-color:#87CEEB; /* barva pro postranní panel*/
+}
+
+th {
+    background-color:#ED2939; /* barva pro záhlaví tabulky */
+    color: #FFFFFF;
+
+/*
+body {
+    background-image: url('sky22.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: repeat;
+}
+*/
+
+}
+.back-link {          /* Tlačítko Home - zpět na Home page - vlevo nahoře */
+    position: absolute;
+    top: 10px;
+    left: 50px;
+    color: blue;
+    text-decoration: none;
+}
+</style>
+
+<body>
+<a href="home.php" class="back-link">Home</a>
+<div class="content">
+<h2 id="Average">Minimum temperature</h2>
+<p id="yearRange">1948 - 2023</p>
+</div>
+<br>
+
+<div class="sidebar"></div>
+
+<h2><img src='sky25.jpg'  alt= 'sky.jpg'style="float:left;width:50px;height:43px;">
+</h2>
+<h2><img src='sky25.jpg'  alt= 'sky.jpg'style="float:left;width:50px;height:43px;">
+</h2>
+
+<?php
+// Počet obrázků k zobrazení
+$numberOfImages = 9;
+?>
+
+<?php for ($i = 0; $i < $numberOfImages; $i++): ?>
+    <h2><img src='sky25.jpg'  alt='sky.jpg' style="float:right;width:50px;height:43px;"></h2>
+<?php endfor; ?>
+
+<?php include 'formmin.php'; ?>
+
+<?php 
+/* Začlenění souboru třídy */
+require_once 'AvgResult.php';
+
+/* Získání vybraného roku a měsíce z URL parametru */
+$selectedYear = isset($_GET['selectedYear']) ? $_GET['selectedYear'] : date('Y');
+$selectedMonth = isset($_GET['selectedMonth']) ? $_GET['selectedMonth'] : 'December';
+
+/* Vytvoření instanci třídy AvgResultAnalysis */
+$AvgResultAnalysis = new AvgResultAnalysis();
+
+/* Získání teplotních dat pro vybraný rok a měsíc */
+$temperatureData = $AvgResultAnalysis->getTemperatureData($selectedYear, $selectedMonth);
+
+/* Zobrazení teplotních dat */
+$AvgResultAnalysis->displayTemperatureData($temperatureData);
+?>
+<!-- Canvas prvek pro graf s identifikátorem (id) -->
+<canvas id="myChart5"></canvas>
+<canvas id="myChart6"></canvas>
+<br>
+</body>
+</html>
