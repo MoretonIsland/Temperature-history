e3a0133c1fc376503a33eafe1ff7798a1093850b
MoretonIsland <107469969+MoretonIsland@users.noreply.github.com>
2024-01-21 11:04:31 +0100

    Create avgdatas.php

---
+++ avgdatas.php
@@ -0,0 +1,640 @@
+<?php
+/* Asociativní pole s zkratkami pro měsíce a roky. 
+   V PHP je asociativní pole reprezentováno pomocí šipek (=>), které spojují klíče
+   a hodnoty.  
+   Každý klíč představuje zkratku roku (např. '1948' pro 'July 1948') a je přiřazen
+   k odpovídajícímu údaji o průměrné teplotě v červenci v daném roce.
+   Příklad:
+   - Klíč '1948' (zkratka) odpovídá údajům o průměrné teplotě v červenci 1948.
+   - Klíč '1949' (zkratka) odpovídá údajům o průměrné teplotě v červenci 1949.
+   Asociativní pole umožňuje snadný přístup k údajům pomocí jejich zkratky.
+   To usnadňuje manipulaci s daty a uspořádání informací. */
+$dateAbbreviations = [
+        '1948' => 'July 1948',
+        '1949' => 'July 1949',
+        '1950' => 'July 1950',
+        '1951' => 'July 1951',
+        '1952' => 'July 1952',
+        '1953' => 'July 1953',
+        '1954' => 'July 1954',
+        '1955' => 'July 1955',
+        '1956' => 'July 1956',
+        '1957' => 'July 1957',
+        '1958' => 'July 1958',
+        '1959' => 'July 1959',
+        '1960' => 'July 1960',
+        '1961' => 'July 1961',
+        '1962' => 'July 1962',
+        '1963' => 'July 1963',
+        '1964' => 'July 1964',
+        '1965' => 'July 1965',
+        '1966' => 'July 1966',
+        '1967' => 'July 1967',
+        '1968' => 'July 1968',
+        '1969' => 'July 1969',
+        '1970' => 'July 1970',
+        '1971' => 'July 1971',
+        '1972' => 'July 1972',
+        '1973' => 'July 1973',
+        '1974' => 'July 1974',
+        '1975' => 'July 1975',
+        '1976' => 'July 1976',
+        '1977' => 'July 1977',
+        '1978' => 'July 1978',
+        '1979' => 'July 1979',
+        '1980' => 'July 1980',
+        '1981' => 'July 1981',
+        '1982' => 'July 1982',
+        '1983' => 'July 1983',
+        '1984' => 'July 1984',
+        '1985' => 'July 1985',
+        '1986' => 'July 1986',
+        '1987' => 'July 1987',
+        '1988' => 'July 1988',
+        '1989' => 'July 1989',
+        '1990' => 'July 1990',
+        '1991' => 'July 1991',
+        '1992' => 'July 1992',
+        '1993' => 'July 1993',
+        '1994' => 'July 1994',
+        '1995' => 'July 1995',
+        '1996' => 'July 1996',
+        '1997' => 'July 1997',
+        '1998' => 'July 1998',
+        '1999' => 'July 1999',
+        '2000' => 'July 2000',
+        '2001' => 'July 2001',
+        '2002' => 'July 2002',
+        '2003' => 'July 2003',
+        '2004' => 'July 2004',
+        '2005' => 'July 2005',
+        '2006' => 'July 2006',
+        '2007' => 'July 2007',
+        '2008' => 'July 2008',
+        '2009' => 'July 2009',
+        '2010' => 'July 2010',
+        '2011' => 'July 2011',
+        '2012' => 'July 2012',
+        '2013' => 'July 2013',
+        '2014' => 'July 2014',
+        '2015' => 'July 2015',
+        '2016' => 'July 2016',
+        '2017' => 'July 2017',
+        '2018' => 'July 2018',
+        '2019' => 'July 2019',
+        '2020' => 'July 2020',
+        '2021' => 'July 2021',
+        '2022' => 'July 2022',
+        '2023' => 'July 2023',
+    '1948' => 'December 1948',
+    '1949' => 'December 1949',
+    '1950' => 'December 1950',
+    '1951' => 'December 1951',
+    '1952' => 'December 1952',
+    '1953' => 'December 1953',
+    '1954' => 'December 1954',
+    '1955' => 'December 1955',
+    '1956' => 'December 1956',
+    '1957' => 'December 1957',
+    '1958' => 'December 1958',
+    '1959' => 'December 1959',
+    '1960' => 'December 1960',
+    '1961' => 'December 1961',
+    '1962' => 'December 1962',
+    '1963' => 'December 1963',
+    '1964' => 'December 1964',
+    '1965' => 'December 1965',
+    '1966' => 'December 1966',
+    '1967' => 'December 1967',
+    '1968' => 'December 1968',
+    '1969' => 'December 1969',
+    '1970' => 'December 1970',
+    '1971' => 'December 1971',
+    '1972' => 'December 1972',
+    '1973' => 'December 1973',
+    '1974' => 'December 1974',
+    '1975' => 'December 1975',
+    '1976' => 'December 1976',
+    '1977' => 'December 1977',
+    '1978' => 'December 1978',
+    '1979' => 'December 1979',
+    '1980' => 'December 1980',
+    '1981' => 'December 1981',
+    '1982' => 'December 1982',
+    '1983' => 'December 1983',
+    '1984' => 'December 1984',
+    '1985' => 'December 1985',
+    '1986' => 'December 1986',
+    '1987' => 'December 1987',
+    '1988' => 'December 1988',
+    '1989' => 'December 1989',
+    '1990' => 'December 1990',
+    '1991' => 'December 1991',
+    '1992' => 'December 1992',
+    '1993' => 'December 1993',
+    '1994' => 'December 1994',
+    '1995' => 'December 1995',
+    '1996' => 'December 1996',
+    '1997' => 'December 1997',
+    '1998' => 'December 1998',
+    '1999' => 'December 1999',
+    '2000' => 'December 2000',
+    '2001' => 'December 2001',
+    '2002' => 'December 2002',
+    '2003' => 'December 2003',
+    '2004' => 'December 2004',
+    '2005' => 'December 2005',
+    '2006' => 'December 2006',
+    '2007' => 'December 2007',
+    '2008' => 'December 2008',
+    '2009' => 'December 2009',
+    '2010' => 'December 2010',
+    '2011' => 'December 2011',
+    '2012' => 'December 2012',
+    '2013' => 'December 2013',
+    '2014' => 'December 2014',
+    '2015' => 'December 2015',
+    '2016' => 'December 2016',
+    '2017' => 'December 2017',
+    '2018' => 'December 2018',
+    '2019' => 'December 2019',
+    '2020' => 'December 2020',
+    '2021' => 'December 2021',
+    '2022' => 'December 2022',
+    '2023' => 'December 2023',
+];
+     /* Asociativní pole průměrných teplot pro každý rok v červenci. */
+     return array(
+        'July 1948' => array(
+            '1948' => 22.48,
+        ),
+        'July 1949' => array(
+            '1949' => 23.51,
+        ),
+        'July 1950' => array(
+            '1950' => 20.32,
+        ),
+        'July 1951' => array(
+            '1951' => 20.97,
+        ),
+        'July 1952' => array(
+            '1952' => 23.36,
+        ),
+        'July 1953' => array(
+            '1953' => 22.67,
+        ),
+        'July 1954' => array(
+            '1954' => 21.52,
+        ),
+        'July 1955' => array(
+            '1955' => 24.26,
+        ),
+        'July 1956' => array(
+            '1956' => 19.77,
+        ),
+        'July 1957' => array(
+            '1957' => 21.73,
+        ),
+        'July 1958' => array(
+            '1958' => 20.78,
+        ),
+        'July 1959' => array(
+            '1959' => 21.39,
+        ),
+        'July 1960' => array(
+            '1960' => 19.67,
+        ),
+        'July 1961' => array(
+            '1961' => 21.10,
+        ),
+        'July 1962' => array(
+            '1962' => 19.29,
+        ),
+        'July 1963' => array(
+            '1963' => 21.87,
+        ),
+        'July 1964' => array(
+            '1964' => 22.32,
+        ),
+        'July 1965' => array(
+            '1965' => 21.61,
+        ),
+        'July 1966' => array(
+            '1966' => 23.19,
+        ),
+        'July 1967' => array(
+            '1967' => 20.36,
+        ),
+        'July 1968' => array(
+            '1968' => 21.35,
+        ),
+        'July 1969' => array(
+            '1969' => 19.93,
+        ),
+        'July 1970' => array(
+            '1970' => 22.74,
+        ),
+        'July 1971' => array(
+            '1971' => 20.14,
+        ),
+        'July 1972' => array(
+            '1972' => 20.82,
+        ),
+        'July 1973' => array(
+            '1973' => 21.82,
+        ),
+        'July 1974' => array(
+            '1974' => 22.21,
+        ),
+        'July 1975' => array(
+            '1975' => 22.27,
+        ),
+        'July 1972' => array(
+            '1972' => 20.82,
+        ),
+        'July 1973' => array(
+            '1973' => 21.82,
+        ),
+        'July 1974' => array(
+            '1974' => 22.21,
+        ),
+        'July 1975' => array(
+            '1975' => 22.27,
+        ),
+        'July 1976' => array(
+            '1976' => 22.74,
+        ),
+        'July 1977' => array(
+            '1977' => 22.96,
+        ),
+        'July 1978' => array(
+            '1978' => 20.25,
+        ),
+        'July 1979' => array(
+            '1979' => 21.53,
+        ),
+        'July 1980' => array(
+            '1980' => 21.67,
+        ),
+        'July 1981' => array(
+            '1981' => 19.68,
+        ),
+        'July 1982' => array(
+            '1982' => 21.84,
+        ),
+        'July 1983' => array(
+            '1983' => 24.50,
+        ),
+        'July 1984' => array(
+            '1984' => 22.32,
+        ),
+        'July 1985' => array(
+            '1985' => 22.53,
+        ),
+        'July 1986' => array(
+            '1986' => 22.46,
+        ),
+        'July 1987' => array(
+            '1987' => 23.54,
+        ),
+        'July 1988' => array(
+            '1988' => 24.27,
+        ),
+        'July 1989' => array(
+            '1989' => 21.05,
+        ),
+        'July 1990' => array(
+            '1990' => 19.97,
+        ),     
+        'July 1991' => array(
+            '1991' => 21.98,
+        ),
+        'July 1992' => array(
+            '1992' => 17.39,
+        ),
+        'July 1993' => array(
+            '1993' => 20.56,
+        ),
+        'July 1994' => array(
+            '1994' => 21.21,
+        ),
+        'July 1995' => array(
+            '1995' => 20.74,
+        ),
+        'July 1996' => array(
+            '1996' => 20.45,
+        ),
+        'July 1997' => array(
+            '1997' => 20.42,
+        ),
+        'July 1998' => array(
+            '1998' => 22.46,
+        ),
+        'July 1999' => array(
+            '1999' => 24.66,
+        ),   
+        'July 2000' => array(
+            '2000' => 19.96,
+        ),
+        'July 2001' => array(
+            '2001' => 22.29,
+        ),
+        'July 2002' => array(
+            '2002' => 24.26,
+        ),
+        'July 2003' => array(
+            '2003' => 21.55,
+        ),
+        'July 2004' => array(
+            '2004' => 20.36,
+        ),
+        'July 2005' => array(
+            '2005' => 22.62,
+        ),
+        'July 2006' => array(
+            '2006' => 23.55,
+        ),
+        'July 2007' => array(
+            '2007' => 22.01,
+        ),
+        'July 2008' => array(
+            '2008' => 21.94,
+        ),
+        'July 2009' => array(
+            '2009' => 20.34,
+        ),
+        'July 2010' => array(
+            '2010' => 24.23,
+        ),
+        'July 2011' => array(
+            '2011' => 24.79,
+        ),
+        'July 2012' => array(
+            '2012' => 25.69,
+        ),
+        'July 2013' => array(
+            '2013' => 22.14,
+        ),   
+        'July 2014' => array(
+            '2014' => 20.14,
+        ),
+        'July 2015' => array(
+            '2015' => 21.55,
+        ),
+        'July 2016' => array(
+            '2016' => 23.52,
+        ),
+        'July 2017' => array(
+            '2017' => 22.27,
+        ),
+        'July 2018' => array(
+            '2018' => 23.45,
+        ),
+        'July 2019' => array(
+            '2019' => 23.83,
+        ),
+        'July 2020' => array(
+            '2020' => 24.71,
+        ),   
+        'July 2021' => array(
+            '2021' => 23.03,
+        ),
+        'July 2022' => array(
+            '2022' => 23.44,
+        ),
+        'July 2023' => array(
+            '2023' => 23.14,
+        ),
+           
+    /* Asociativní pole průměrných teplot pro každý rok v prosinci. */
+  
+    'December 1948' => array(
+        '1948' => -2.18,
+    ),
+    'December 1949' => array(
+        '1949' => -1.71,
+    ),
+    'December 1950' => array(
+        '1950' => -7.18,
+    ),
+    'December 1951' => array(
+        '1951' => -4.42,
+    ),
+    'December 1952' => array(
+        '1952' => -0.79,
+    ),
+    'December 1953' => array(
+        '1953' => -1.55,
+    ),
+    'December 1954' => array(
+        '1954' => -1.91,
+    ),
+    'December 1955' => array(
+        '1955' => -5.18,
+    ),
+    'December 1956' => array(
+        '1956' => -1.71,
+    ),
+    'December 1957' => array(
+        '1957' => -1.00,
+    ),
+    'December 1958' => array(
+        '1958' => -7.26,
+    ),
+    'December 1959' => array(
+        '1959' => 0.35,
+    ),
+    'December 1960' => array(
+        '1960' => -4.79,
+    ),
+    'December 1961' => array(
+        '1961' => -4.67,
+    ),
+    'December 1962' => array(
+        '1962' => -5.64,
+    ),
+    'December 1963' => array(
+        '1963' => -10.08,
+    ),
+    'December 1964' => array(
+        '1964' => -4.51,
+    ),
+    'December 1965' => array(
+        '1965' => 0.65,
+    ),
+    'December 1966' => array(
+        '1966' => -3.94,
+    ),
+    'December 1967' => array(
+        '1967' => -1.52,
+    ),
+    'December 1968' => array(
+        '1968' => -3.34,
+    ),
+    'December 1969' => array(
+        '1969' => -3.40,
+    ),
+    'December 1970' => array(
+        '1970' => -3.74,
+    ),
+    'December 1971' => array(
+        '1971' => -1.00,
+    ),
+    'December 1972' => array(
+        '1972' => -6.04,
+    ),
+    'December 1973' => array(
+        '1973' => -3.28,
+    ),
+    'December 1974' => array(
+        '1974' => -1.44,
+    ),
+    'December 1975' => array(
+        '1975' => -1.61,
+    ),
+    'December 1976' => array(
+        '1976' => -8.29,
+    ),
+    'December 1977' => array(
+        '1977' => -4.93,
+    ),
+    'December 1978' => array(
+        '1978' => -4.26,
+    ),
+    'December 1979' => array(
+        '1979' => -0.36,
+    ),
+    'December 1980' => array(
+        '1980' => -4.07,
+    ),
+    'December 1981' => array(
+        '1981' => -4.34,
+    ),
+    'December 1982' => array(
+        '1982' => 0.79,
+    ),
+    'December 1983' => array(
+        '1983' => -9.56,
+    ),
+    'December 1984' => array(
+        '1984' => -1.36,
+    ),
+    'December 1985' => array(
+        '1985' => -8.71,
+    ),
+    'December 1986' => array(
+        '1986' => -1.48,
+    ),
+    'December 1987' => array(
+        '1987' => -0.19,
+    ),
+    'December 1988' => array(
+        '1988' => -2.70,
+    ),
+    'December 1989' => array(
+        '1989' => -8.25,
+    ),
+    'December 1990' => array(
+        '1990' => -2.40,
+    ),
+    'December 1991' => array(
+        '1991' => -1.36,
+    ),
+    'December 1992' => array(
+        '1992' => -1.91,
+    ),
+    'December 1993' => array(
+        '1993' => -1.07,
+    ),
+    'December 1994' => array(
+        '1994' => 1.22,
+    ),
+    'December 1995' => array(
+        '1995' => -3.46,
+    ),
+    'December 1996' => array(
+        '1996' => -2.50,
+    ),
+    'December 1997' => array(
+        '1997' => -0.87,
+    ),
+    'December 1998' => array(
+        '1998' => 0.03,
+    ),
+    'December 1999' => array(
+        '1999' => -1.52,
+    ),
+    'December 2000' => array(
+        '2000' => -8.06,
+    ),
+    'December 2001' => array(
+        '2001' => 0.59,
+    ),
+    'December 2002' => array(
+        '2002' => -0.92,
+    ),
+    'December 2003' => array(
+        '2003' => -0.40,
+    ),
+    'December 2004' => array(
+        '2004' => -2.17,
+    ),
+    'December 2005' => array(
+        '2005' => -4.69,
+    ),
+    'December 2006' => array(
+        '2006' => 0.64,
+    ),
+    'December 2007' => array(
+        '2007' => -2.74,
+    ),
+    'December 2008' => array(
+        '2008' => -4.84,
+    ),
+    'December 2009' => array(
+        '2009' => -2.63,
+    ),
+    'December 2010' => array(
+        '2010' => -3.86,
+    ),
+    'December 2011' => array(
+        '2011' => 0.95,
+    ),
+    'December 2012' => array(
+        '2012' => 1.20,
+    ),
+    'December 2013' => array(
+        '2013' => -5.53,
+    ),
+    'December 2014' => array(
+        '2014' => -0.17,
+    ),
+    'December 2015' => array(
+        '2015' => 3.02,
+    ),
+    'December 2016' => array(
+        '2016' => -3.01,
+    ),
+    'December 2017' => array(
+        '2017' => -3.30,
+    ),
+    'December 2018' => array(
+        '2018' => 0.26,
+    ),
+    'December 2019' => array(
+        '2019' => 0.94,
+    ),
+    'December 2020' => array(
+        '2020' => 0.17,
+    ),
+    'December 2021' => array(
+        '2021' => 2.37,
+    ),
+    'December 2022' => array(
+        '2022' => -1.09,
+    ),
+    'December 2023' => array(
+        '2023' => 4.67,
+    ),
+);
+?>   
